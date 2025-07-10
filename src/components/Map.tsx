a2f744c6922783de8237a97220346fac5ff3ac93
Sanchez <sanchez@Sanchezs-MacBook-Pro.local>
2025-07-10 15:08:55 +0800

    mobile responsive map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,16 +3,17 @@ import React from "react";
 const Map = () => {
   return (
     <div className="w-full flex justify-center items-center">
-      <iframe
-        width="600"
-        height="450"
-        style={{ border: 0 }}
-        loading="lazy"
-        allowFullScreen
-        referrerPolicy="no-referrer-when-downgrade"
-        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDeqX1AQ9oLUuBk_aY3P4TtxTpjtv-RdRs&q=133+New+Bridge+Rd,+#14-10,+Singapore+059413"
-        title="Google Map"
-      ></iframe>
+      <div className="w-full max-w-[600px] aspect-[4/3]">
+        <iframe
+          className="w-full h-full"
+          style={{ border: 0 }}
+          loading="lazy"
+          allowFullScreen
+          referrerPolicy="no-referrer-when-downgrade"
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDeqX1AQ9oLUuBk_aY3P4TtxTpjtv-RdRs&q=133+New+Bridge+Rd,+#14-10,+Singapore+059413"
+          title="Google Map"
+        ></iframe>
+      </div>
     </div>
   );
 };
